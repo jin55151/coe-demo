430abd7c5ec56a50c6d1764d46a5f3204d7e29b5
Linh PC <linhvn.rails@gmail.com>
2024-10-28 15:26:11 +0700

    Add cloud eureka and cloud ribbon demo

---
+++ cloud-ribbon/word-server-solution/run-all.sh
@@ -0,0 +1,5 @@
+./mvnw spring-boot:run -Dspring-boot.run.profiles=subject &
+./mvnw spring-boot:run -Dspring-boot.run.profiles=verb &
+./mvnw spring-boot:run -Dspring-boot.run.profiles=article &
+./mvnw spring-boot:run -Dspring-boot.run.profiles=adjective &
+./mvnw spring-boot:run -Dspring-boot.run.profiles=noun 
